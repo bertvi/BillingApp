7f1d22bf1f52566657a8b65cb3dbeea12216312d
bertvi <bert.viikmae@gofore.com>
2021-11-27 13:39:05 +0200

    add views

--- bills/views.py
+++ bills/views.py
@@ -1,5 +1,3 @@
-from datetime import datetime
-
 from django.http import HttpResponse
 from django.template import loader
 
@@ -8,14 +6,6 @@ from bills.models import Billing
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
-
-def new(request, data_dict):
-    billing = Billing(created=datetime.datetime.now(),
-                      changed=None,
-                      date=data_dict['date'],
-                      due_date=data_dict['due_date'],
-                      reference=data_dict['reference'])
-    billing.save()
 
 def detail(request, bill_id):
     bill = Billing.objects.get(id=bill_id)
